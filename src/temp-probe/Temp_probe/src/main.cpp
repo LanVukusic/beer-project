65e2ba5435b7e13474d193f75773a65cc5a732a7
Lan Vukušič <lan.vukusic@gmail.com>
2023-03-24 21:06:41 +0100

    working probe temp with display on arduino nano

--- src/temp-probe/Temp_probe/src/main.cpp
+++ src/temp-probe/Temp_probe/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#define ONE_WIRE_BUS 4
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+
+#define ONE_WIRE_BUS 5
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup()
 {
 
   Serial.begin(9600);
-
   sensors.begin();
 
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+  delay(500);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  // Display static text
+  display.println("loading..");
+  display.display();
 }
 
 void loop()
@@ -20,5 +42,38 @@ void loop()
   sensors.requestTemperatures();
   float temp = sensors.getTempCByIndex(0);
   Serial.println(temp);
-  delay(1000);
+  delay(500);
+
+  if (temp - (-127.0) < 0.0001)
+  {
+    Serial.println("No sensor found");
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    // Display static text
+    display.println("No sensor found");
+    display.display();
+    delay(5000);
+    return;
+  }
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  // Display static text
+  display.println("Temp:");
+
+  display.setTextSize(3);
+  display.setCursor(0, 30);
+  // display temperture with 2 decimal places and a degree symbol
+  display.println(temp);
+
+  // frontend is my passion
+  display.drawCircle(110, 34, 3, WHITE);
+  display.setCursor(115, 30);
+  display.setTextSize(2);
+  display.println("C");
+  display.display();
 }
